b567bc676a99572794bc552f8d22a2adc0a7a038
Joe Dougherty <joseph.dougherty@gmail.com>
2017-12-13 11:49:51 -0500

    Attempting to convert to Ansible playbook

--- joy-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INSTALL_DIR=~/joy-download
-
-mkdir -p $INSTALL_DIR && cd $INSTALL_DIR
-wget http://www.kevinalbrecht.com/code/joy-mirror/joy.tar.gz
-tar -xvzf joy.tar.gz
-make
-mkdir -p ~/bin && cd ~/bin && ln -s $INSTALL_DIR/joy joy
